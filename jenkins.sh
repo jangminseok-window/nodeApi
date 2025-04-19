88f29aab8d91cdd2870bfbc7ce8089debd31fb17
midstar <midstar.jang@gmail.com>
2025-04-19 14:04:08 +0900

    준영메디 소스 추가 예정

--- jenkins.sh
+++ jenkins.sh
@@ -5,6 +5,7 @@
 nohup env NODE_ENV=production /home/ubuntu/.nvm/versions/node/v22.9.0/bin/node apiApp.js > /dev/null 2>&1 &
 
 # 또는 Apache 재시작
-sudo systemctl restart apache2
+ sudo service nginx restart
+
 
 echo "Deployment completed and production api services restarted"
